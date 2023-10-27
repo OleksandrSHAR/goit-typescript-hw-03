e718df27e4c43cf9f4ed5a2ec8f3760354311c0d
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2023-10-27 17:43:42 +0300

    home3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,56 @@
+
+class Key{
+    private signature: number
+    constructor() {
+        this.signature=Math.random()
+    }
+    getSignature():number {
+         return this.signature
+    }
+      
+    
+}
+class Person{
+    private  key:Key
+    getKey() {
+         return this.key
+    }
+    constructor(key: Key) {
+        this.key=key
+    }
+}
+abstract class House {
+    door: boolean = false
+    key: Key
+    comeln: Person[] = []
+
+    constructor(key: Key) {
+        this.key=key
+    }
+ comeIn(person: Person) {
+        if (this.door) {
+            this. comeln.push(person)
+            console.log('The door is open,welcome')
+        }
+        console.log('The door could not be opened')
+    }
+     
+    abstract openDoor(key: Key):void      
+     
+}
+class MyHouse extends House{
+     
+     openDoor(key: Key) :void  {
+        if (key.getSignature()===this.key.getSignature()) {
+            this.door = true
+            console.log('The door is open ')
+         } else{ console.log('the key does not fit, maybe another one?') }
+        
+     }
+     
+        
+}
+ 
 const key = new Key();
 
 const house = new MyHouse(key);
